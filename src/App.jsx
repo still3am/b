0d443c6e51e57eed84cced11740858716f4a5a8b
Denzel Valdez <denzelvaldez22@gmail.com>
2026-04-16 08:35:02 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import Layout from './components/Layout.jsx';
+import Layout from './components/Layout';
 
 // Pages
 import Home from './pages/Home';
